9f26f08c30e44d9824e27ca3833082c63b6946c1
Kalyan <Kalyan@DESKTOP-HIGVP14>
2022-06-17 11:50:53 +0530

    kalyan:19 june 2022:adding the fruits prices

---
+++ src/fruitprices/fruits1.java
@@ -0,0 +1,20 @@
+package fruitprices;
+
+public class price {
+	static int apples=99;
+	public static void main(String[] args) {
+		 if(apples>=100) {
+			 System.out.println("print the statement is true:");
+		 }
+		 else if(apples<=100) {
+			 System.out.println("print the statement is false:");
+		 }
+		 else if (apples>=1000) {
+			 System.out.println("print the statement is none:");
+		 }
+		 else if (apples<=10) {
+			 System.out.println("print the statement is correct:");
+		 }
+	}
+
+}
